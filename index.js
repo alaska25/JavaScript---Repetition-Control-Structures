8d2e8ee857f6a289d4ed8daabad2dc7a6d35b821
Arman <villegasarman23@gmail.com>
2022-06-01 06:32:01 -0700

    Added new code

---
+++ index.js
@@ -0,0 +1,42 @@
+// console.log("Hello World");
+
+
+//3. Create a variable number that will store the value of the number provided by the user via the prompt.
+
+console.log("The number you provided is 100.");
+let number = Number(prompt("Please provide a number"));
+do {
+	console.log("Do-while: " +number);
+	// number++ //increament pusposes
+	// number = number + 1; 
+	number += 1;
+
+	// loop will stop if the number is equal or greater than 10.
+} while(number < 10);
+
+
+// 4. Create a for loop that will be initialized with the number provided by the user, will stop when the value is less than or equal to 0 and will decrease by 1 every iteration.
+
+
+
+// 5. Create a condition that if the current value is less than or equal to 50, stop the loop.
+
+
+// 6. Create another condition that if the current value is divisible by 10, print a message that the number is being skipped and continue to the next iteration of the loop.
+
+
+
+// 7. Create another condition that if the current value is divisible by 5, print the number
+
+
+// 8. Create a variable that will contain the string supercalifragilisticexpialidocious.
+
+
+// 9. Create another variable that will store the consonants from the string.
+
+
+// 10. Create another for Loop that will iterate through the individual letters of the string based on it’s length.
+
+// 11. Create an if statement that will check if the letter of the string is equal to a vowel and continue to the next iteration of the loop if it is true.
+
+// 12. Create an else statement that will add the letter to the second variable.
